0394ad63cc4b2b3aeed8b675e3a9706ba1e016f2
Dhoyralves <dhoyralves@gmail.com>
2023-03-22 23:52:10 -0300

    Interpolation Class

--- Fundamentals-Angular/src/app/primeiroComponent/primeiroComponent.component.ts
+++ Fundamentals-Angular/src/app/primeiroComponent/primeiroComponent.component.ts
@@ -12,3 +12,8 @@ export class PrimeiroComponenteComponent{}
 
 
 //a ordem é criar um @componente com esses parametros, depois ir para app.module.ts e adcionar a exporr class nas declarations e depois no html adcionar o seletor como tag
+
+// para criar um segundo component de uma só com apenas um comando digitamos: ng generate component nomeDoNOVOComponente
+
+//o ultimo arquivo nomeDoNOVOComponente.component.ts é a classe do componente; temos um .spec.ts que é para testes; o .html é o template e o .css para stylos
+
