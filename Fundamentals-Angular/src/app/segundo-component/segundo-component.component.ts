37dde9a29fdd5df782c3509b417d886bc91a5d91
Dhoyralves <dhoyralves@gmail.com>
2023-03-23 17:35:22 -0300

    Use of Property binding e event binding and aspas

--- Fundamentals-Angular/src/app/segundo-component/segundo-component.component.ts
+++ Fundamentals-Angular/src/app/segundo-component/segundo-component.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class SegundoComponentComponent {
   nome1 = "Dhoy"; //interpolação de texto, para passar algo da classe para o html
   dataNascimento = "1998-01-01";
+
+  urlImagem = "/assets/foca.jpg"; // as imagens ficam no  arquivo assets, aqui é para o uso do property
+
+  mostrarDataNascimento (){
+    alert(`A data de nascimento: ${this.dataNascimento}`); //cuidado com as aspas, pois elas são invertidas (clases), existem 3 tipos de aspas... "para chamar uma função pai", 'para chamar a função filha de dentro da pai' e a para interpolação que é `interpolação, para concatenar dados`
+  }
 }
+// o property binding liga as propreidades de tags do html com as proriedaes dos componentes ex.:urlImage
+//Event binding ligar os metodos do meu componente as ações/eventos que acontecem no html
