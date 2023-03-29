9f2e678e0a38f42c10be28146750f5cd0f6e2d5f
Dhoyralves <dhoyralves@gmail.com>
2023-03-29 00:36:35 -0300

    Parent-child component intetaction

---
+++ Fundamentals-Angular/src/app/componente-filho/componente-filho.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-filho',
+  templateUrl: './componente-filho.component.html',
+  styleUrls: ['./componente-filho.component.css']
+})
+export class ComponenteFilhoComponent {
+  @Input() sobrenomeDoPai1 = ""; //aqui atraves desse decorador @ ele vai receber o sobrenome do pai //atraves do input inserimos informações dentro do componente
+  nome11 = ""; // aqui recebe a propriedade do html two-way [(ngModel)]
+
+
+
+  
+}
---
+++ Fundamentals-Angular/src/app/componente-pai/componente-pai.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-pai',
+  templateUrl: './componente-pai.component.html',
+  styleUrls: ['./componente-pai.component.css']
+})
+export class ComponentePaiComponent {
+  sobrenomeDoPai = "Alves"; //vou passar esse sobrenome do pai pro componente filho...
+
+
+  mostrarNomeCompleto(nomeCompleto: any){
+    alert(`O nome completo de filho para pai é: ${nomeCompleto}`);
+  } //evento output, serve para emitir/disparar eventos/funções que estao definidas no elemento pai, onde vou disparar o metodo mostrarNomeCompleto que esta definido no componente pai atraves do elemento filho
+}
