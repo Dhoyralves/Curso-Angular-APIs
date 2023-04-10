91732c78095e94424eab59b155a95ba73455b2bd
Dhoyralves <dhoyralves@gmail.com>
2023-04-09 21:50:32 -0300

    cards

--- Fundamentals-Angular/src/app/card/card.component.ts
+++ Fundamentals-Angular/src/app/card/card.component.ts
@@ -9,8 +9,5 @@ export class CardComponent {
 
     @Input() titulo = "";
     @Input() cor = "#ccc"; //caso ele n receba nenhum parametro ele recebe essa cor
-
-
 }
-
 //elemento ngcontent, usado para reutilizar nosso codigo, quando precisamos colocar componentes dentro de outros, colocando nossos templates dentro desses containers
